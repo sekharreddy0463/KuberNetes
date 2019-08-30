316546c5e9720a84e78488e2631e9315124b1c06
andalike <43137695+andalike@users.noreply.github.com>
2019-08-30 22:03:59 +0530

    Add files via upload

---
+++ dockerMaster.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt install -y docker.io
+sudo systemctl enable docker
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt install -y curl
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt install -y kubeadm
+sudo swapoff -a
+sudo hostnamectl set-hostname master-node
