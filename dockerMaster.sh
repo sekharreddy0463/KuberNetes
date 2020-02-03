53daf95246bc38142e0af4810135a745311ad64f
andalike <43137695+andalike@users.noreply.github.com>
2020-02-03 19:00:02 +0530

    Kuberadm install

--- dockerMaster.sh
+++ dockerMaster.sh
@@ -7,3 +7,7 @@ sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt install -y kubeadm
 sudo swapoff -a
 sudo hostnamectl set-hostname master-node
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
